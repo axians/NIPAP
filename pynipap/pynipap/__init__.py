550a8f3d57a432894a6a362026f7b5e88972533d
soundgoof <soundgoof@gmail.com>
2026-09-24 15:48:40 +0200

    release: prepare Axians 5 with success message contrast fixes

--- pynipap/pynipap/__init__.py
+++ pynipap/pynipap/__init__.py
@@ -1,6 +1,6 @@
 from .pynipap import *
 
-__version__     = "0.32.7+axians.4"
+__version__     = "0.32.7+axians.5"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__= "kll@tele2.net, lukas@spritelink.net"
 __copyright__   = "Copyright 2011, Kristian Larsson, Lukas Garberg"
